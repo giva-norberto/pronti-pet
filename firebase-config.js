da609813ce621e798f38f04630c7138795a50623
giva-norberto <GIVA.NORBERTO@GMAIL.COM>
2026-06-08 17:17:56 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // ======================================================================
-// ARQUIVO: firebase-config.js (PAINEL - VERSÃO 100% ESTÁVEL)
-// =====================================================================
+// ARQUIVO: firebase-config.js
+// PRONTI PET
+// ======================================================================
 
 import { initializeApp, getApp, getApps } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
@@ -8,28 +9,25 @@ import { getAuth, setPersistence, browserLocalPersistence, GoogleAuthProvider } 
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkJt49sM3n_hIQOyEwzgOmzzdPlsF9PW4",
-  authDomain: "pronti-app-37c6e.firebaseapp.com",
-  projectId: "pronti-app-37c6e",
-  storageBucket: "pronti-app-37c6e.firebasestorage.app",
-  messagingSenderId: "736700619274",
-  appId: "1:736700619274:web:557aa247905e56fa7e5df3"
+  apiKey: "AIzaSyDxbb2_onT2gbQahqogcddCOjNTWbwjb0k",
+  authDomain: "pronti-pet.firebaseapp.com",
+  projectId: "pronti-pet",
+  storageBucket: "pronti-pet.firebasestorage.app",
+  messagingSenderId: "970443692765",
+  appId: "1:970443692765:web:21b8e61ff165f36e46d934"
 };
 
-const getFirebaseApp = ( ) => {
-  return getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-};
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-const app = getFirebaseApp();
 const auth = getAuth(app);
 const storage = getStorage(app);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-  prompt: 'select_account'
+  prompt: "select_account"
 });
 
-const db = getFirestore(app, "pronti-app");
+const db = getFirestore(app);
 
 setPersistence(auth, browserLocalPersistence);
 
